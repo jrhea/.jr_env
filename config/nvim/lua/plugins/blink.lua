91a5f14086597e9cfaef28d2567185507f89863e
jonny rhea <jonathan.rhea@gmail.com>
2026-02-07 14:32:25 -0600

    only autocomplete with C- <space>

---
+++ config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        -- only show completion menu when manually triggered (e.g. <C-space>)
+        auto_show = false,
+      },
+    },
+  },
+}
